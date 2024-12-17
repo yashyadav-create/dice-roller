90a258069dfd9f7eef38f9d8e08c90036bb971d7
Yash Yadav <yashyadav88690@gmail.com>
2024-12-17 15:51:40 +0530

    final changes done all functionalities added

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-let result=document.getElementById("result");
-let diceImages =  document.getElementById("diceResult");
-function diceydice(){
 
+function diceydice(){
+    let diceResult =  document.getElementById("diceResult");
+    let diceImages = document.getElementById("diceImages")
+    let inputValue = document.getElementById("inputValue").value;
+    let images=[];
+    let temp=[];
+    for(let i=0;i<inputValue;i++){
+     let rolled=Math.floor(Math.random()*6)+1;
+     temp.push(rolled);
+     images.push(`<img src="dice_images/${rolled}.png">`);
+    }
+    diceResult.textContent = `dice :${temp.join(",")}`;
+    diceImages.innerHTML = images.join(" ");
 }
